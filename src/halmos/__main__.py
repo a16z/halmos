37afca238d989783aefe4de2059b114a5c742534
Daejun Park <daejunpark@gmail.com>
2023-05-09 20:55:57 -0700

    feat: do not set default storage values to symbolic

--- src/halmos/__main__.py
+++ src/halmos/__main__.py
@@ -34,6 +34,7 @@ def parse_args(args) -> argparse.Namespace:
     parser.add_argument('--depth', metavar='MAX_DEPTH', type=int, help='set the max path length')
     parser.add_argument('--array-lengths', metavar='NAME1=LENGTH1,NAME2=LENGTH2,...', help='set the length of dynamic-sized arrays including bytes and string (default: loop unrolling bound)')
 
+    parser.add_argument('--symbolic-storage', action='store_true', help='set default storage values to symbolic')
     parser.add_argument('--symbolic-jump', action='store_true', help='support symbolic jump destination (experimental)')
 
     parser.add_argument('--no-smt-add',          action='store_true', help='do not interpret `+`')
@@ -202,7 +203,7 @@ def run_bytecode(hexcode: str, args: argparse.Namespace, options: Dict) -> List[
         callvalue = callvalue,
         caller    = caller,
         this      = this,
-        symbolic  = True,
+        symbolic  = args.symbolic_storage,
         solver    = solver,
     )
     (exs, _) = sevm.run(ex)
@@ -332,7 +333,7 @@ def run(
         st        = State(),
         jumpis    = {},
         output    = None,
-        symbolic  = True,
+        symbolic  = args.symbolic_storage,
         prank     = Prank(), # prank is reset after setUp()
         #
         solver    = solver,
