79dcec939c1a1027f9fca036d11f3acf464809b0
Daejun Park <daejunpark@gmail.com>
2023-05-24 19:33:28 -0700

    fix: z3 external process query

--- src/halmos/__main__.py
+++ src/halmos/__main__.py
@@ -472,9 +472,10 @@ def gen_model(args: argparse.Namespace, models: List, idx: int, ex: Exec) -> Non
         fname = f'/tmp/{uuid.uuid4().hex}.smt2'
         if args.verbose >= 4: print(f'z3 -smt2 {fname}')
         with open(fname, 'w') as f:
-            f.write('(set-logic QF_AUFBV)\n')
+        #   f.write('(set-logic QF_AUFBV)\n')
             f.write(ex.solver.to_smt2())
-        res_str = subprocess.run(['z3', fname], capture_output=True, text=True).stdout.strip()
+        #   f.write('(assert (forall ((x (_ BitVec 256)) (y (_ BitVec 256))) (= (evm_div x y) (ite (= y (_ bv0 256)) (_ bv0 256) (bvudiv x y)))))') # (evm_div x y) == (ite (y == 0) 0 (bvudiv x y))
+        res_str = subprocess.run(['z3', '-model', fname], capture_output=True, text=True).stdout.strip()
         if args.verbose >= 4: print(res_str)
         if res_str == 'unsat':
             res = unsat
