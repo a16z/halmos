427532c4c371439a97cf3cafaed7c74f4bcd0d59
Daejun Park <daejunpark@gmail.com>
2023-05-25 10:34:04 -0700

    update comments

--- src/halmos/__main__.py
+++ src/halmos/__main__.py
@@ -472,11 +472,10 @@ def gen_model(args: argparse.Namespace, models: List, idx: int, ex: Exec) -> Non
         fname = f'/tmp/{uuid.uuid4().hex}.smt2'
         if args.verbose >= 4: print(f'z3 -smt2 {fname}')
         query = ex.solver.to_smt2()
-        query = query.replace('(evm_div', '(bvudiv')
+        query = query.replace('(evm_div', '(bvudiv') # TODO: replace `(evm_div x y)` with `(ite (= y (_ bv0 256)) (_ bv0 256) (bvudiv x y))` as bvudiv is undefined when y = 0
         with open(fname, 'w') as f:
-        #   f.write('(set-logic QF_AUFBV)\n')
+        #   f.write('(set-logic QF_AUFBV)\n') # generated queries may include non smtlib2 symbols, like const arrays
             f.write(query)
-        #   f.write('(assert (forall ((x (_ BitVec 256)) (y (_ BitVec 256))) (= (evm_div x y) (ite (= y (_ bv0 256)) (_ bv0 256) (bvudiv x y)))))') # (evm_div x y) == (ite (y == 0) 0 (bvudiv x y))
         res_str = subprocess.run(['z3', '-model', fname], capture_output=True, text=True).stdout.strip()
         if args.verbose >= 4: print(res_str)
         if res_str == 'unsat':
