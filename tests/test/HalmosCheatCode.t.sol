5d47f02a319aa4abdc7a67ec9cc608436463318b
Daejun Park <daejunpark@gmail.com>
2023-07-17 22:50:29 -0700

    update tests

--- tests/test/HalmosCheatCode.t.sol
+++ tests/test/HalmosCheatCode.t.sol
@@ -1,32 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-interface SVM {
-    // Create a new symbolic uint value ranging over [0, 2**bitSize - 1] (inclusive)
-    function createUint(uint256 bitSize, string memory name) external returns (uint256 value);
-
-    // Create a new symbolic byte array with the given byte size
-    function createBytes(uint256 byteSize, string memory name) external returns (bytes memory value);
-
-    // Create a new symbolic uint256 value
-    function createUint256(string memory name) external returns (uint256 value);
-
-    // Create a new symbolic bytes32 value
-    function createBytes32(string memory name) external returns (bytes32 value);
-
-    // Create a new symbolic address value
-    function createAddress(string memory name) external returns (address value);
-
-    // Create a new symbolic boolean value
-    function createBool(string memory name) external returns (bool value);
-}
-
-abstract contract SymTest {
-    // SVM cheat code address: 0xf3993a62377bcd56ae39d773740a5390411e8bc9
-    address internal constant SVM_ADDRESS = address(uint160(uint256(keccak256("svm cheat code"))));
-
-    SVM internal constant svm = SVM(SVM_ADDRESS);
-}
+import {SymTest} from "halmos-cheatcodes/SymTest.sol";
 
 contract HalmosCheatCodeTest is SymTest {
     function checkSymbolicUint() public {
