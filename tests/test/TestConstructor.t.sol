ee7c366807a1d7a870e3dce5fe78d44a77881fd7
Daejun Park <daejunpark@gmail.com>
2023-08-03 14:11:24 -0700

    update test

--- tests/test/TestConstructor.t.sol
+++ tests/test/TestConstructor.t.sol
@@ -2,11 +2,20 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract TestConstructorTest {
-    uint public value = 1;
+    uint initialized = 1;
     uint constant const = 2;
+    uint immutable flag;
+    uint value;
+
+    constructor () {
+        flag = 3;
+        value = 4;
+    }
 
     function check_value() public view {
-        assert(value == 1);
+        assert(initialized == 1);
         assert(const == 2);
+        assert(flag == 3);
+        assert(value == 4);
     }
 }
