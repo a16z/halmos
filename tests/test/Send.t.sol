061e248f5c260eda0b0bc11716b95b5e8aa71013
Daejun Park <daejunpark@gmail.com>
2023-08-15 11:10:57 -0700

    update send test

--- tests/test/Send.t.sol
+++ tests/test/Send.t.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
+contract C {
+    constructor() payable { }
+}
+
 /// @custom:halmos --solver-timeout-assertion 0
 contract SendTest {
 
@@ -66,6 +70,38 @@ contract SendTest {
                 assert(newBalanceSender == oldBalanceSender);
                 assert(newBalanceReceiver == oldBalanceReceiver);
             }
+            assert(oldBalanceSender + oldBalanceReceiver == newBalanceSender + newBalanceReceiver);
+            assert(oldBalanceOthers == newBalanceOthers);
+        }
+    }
+
+    function check_create(address receiver, uint amount, address others, bytes32 salt, uint mode) public {
+        require(others != address(this) && others != receiver);
+
+        uint oldBalanceSender = address(this).balance;
+        uint oldBalanceReceiver = receiver.balance;
+        uint oldBalanceOthers = others.balance;
+
+        C c;
+        if (mode == 0) {
+            c = new C{ value: amount }();
+        } else {
+            c = new C{ value: amount, salt: salt }();
+        }
+        require(receiver == address(c));
+
+        assert(oldBalanceSender >= amount);
+
+        uint newBalanceSender = address(this).balance;
+        uint newBalanceReceiver = receiver.balance;
+        uint newBalanceOthers = others.balance;
+
+        unchecked {
+            assert(receiver != address(this)); // new address cannot be equal to the deployer
+            assert(newBalanceSender == oldBalanceSender - amount);
+            assert(newBalanceSender <= oldBalanceSender);
+            assert(newBalanceReceiver == oldBalanceReceiver + amount);
+            assert(newBalanceReceiver >= oldBalanceReceiver);
             assert(oldBalanceSender + oldBalanceReceiver == newBalanceSender + newBalanceReceiver);
             assert(oldBalanceOthers == newBalanceOthers);
         }
