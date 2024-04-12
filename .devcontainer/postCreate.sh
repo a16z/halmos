2872407336bcc3d9f00d61c2f4cbcb05f7223bbc
Cosmin Radoi <cosmin.radoi@gmail.com>
2024-04-12 02:41:53 +0000

    finally working

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,3 +6,12 @@ python -m pip install -r requirements-dev.txt
 pre-commit install
 pre-commit run --all-files
 python -m pip install -e .
+
+deactivate
+mkdir foundry
+cd foundry
+curl -L https://foundry.paradigm.xyz | bash
+/home/vscode/.foundry/bin/foundryup
+
+# just a test
+# pytest -v -k "not long and not ffi" --ignore=tests/lib --halmos-options="-v -st --storage-layout solidity --solver-timeout-assertion 0"
