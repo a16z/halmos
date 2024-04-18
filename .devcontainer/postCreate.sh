695fb5312d2fa4fc8f7301505c6baecf4b8bdb0a
karmacoma <karma@coma.lol>
2024-04-18 15:46:54 -0700

    drop pip install -r requirements.txt

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 python -m venv .venv
 source .venv/bin/activate
-python -m pip install -r requirements.txt
+python -m pip install -e .
 python -m pip install -r requirements-dev.txt
 pre-commit install
 pre-commit run --all-files
-python -m pip install -e .
 
 # Installing during postCreateCommand instead of at docker creation
 # because foundryup installs the binaries in place not accessible by the
