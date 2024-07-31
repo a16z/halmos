21c920d3be397b9e72ad780c0101ae0b7dce8beb
karmacoma <karma@coma.lol>
2024-07-31 10:10:15 -0700

    add test_mapper.test_parse_multicontract_ast

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,8 +1,24 @@
 from typing import List
 
+import json
+import os
 import pytest
 
-from halmos.mapper import AstNode, ContractMappingInfo, Mapper, SingletonMeta
+from halmos.mapper import AstNode, Mapper, SingletonMeta
+
+
+@pytest.fixture
+def read_json_file(request):
+    """fixture to read json files under tests/data"""
+
+    def _read_file(filename):
+        # Get the directory of the current test file
+        test_dir = request.fspath.dirname
+        file_path = os.path.join(test_dir, "data", filename)
+        with open(file_path, "r") as file:
+            return json.load(file)
+
+    return _read_file
 
 
 @pytest.fixture
@@ -177,3 +193,56 @@ def test_parse_complex_ast(mapper):
     assert "func1" in node_names
     assert "event1" in node_names
     assert "error1" in node_names
+
+
+def test_parse_multicontract_ast(read_json_file, mapper):
+    json_data = read_json_file("multi-contract-ast.json")
+    mapper.parse_ast(json_data["ast"])
+
+    # the following are not in contract scope:
+    #
+    #   event Log(uint256);
+    #   error Unauthorized();
+    #   function someFreeFunc() pure returns (uint256) {
+    #       return 42;
+    #   }
+    #
+    # but the free function is not added to the mapper (it has no selector)
+    not_in_contract = mapper.get_by_name(None)
+    assert len(not_in_contract.nodes) == 2
+    assert not_in_contract.nodes[0].name == "Log"
+    assert (
+        not_in_contract.nodes[0].address
+        == "0x909c57d5c6ac08245cf2a6de3900e2b868513fa59099b92b27d8db823d92df9c"
+    )
+    assert not_in_contract.nodes[1].name == "Unauthorized"
+    assert not_in_contract.nodes[1].address == "0x82b42900"
+
+    # there are 2 contracts in the AST
+    # this would be visible as:
+    # - out/TestA.sol/TestA.json (AST includes both TestA and C)
+    # - out/TestA.sol/C.json (AST includes both TestA and C)
+
+    # 3 total scopes, including the non-contract scope
+    assert len(mapper._contracts) == 3
+
+    # contract TestA {
+    #    function internal_func() internal pure {}
+    #    function test_foo() public view returns (uint256) { ... }
+    # }
+    #
+    # the internal function is not added to the mapper (it has no selector)
+    contract_TestA = mapper.get_by_name("TestA")
+    assert contract_TestA is not None
+    assert len(contract_TestA.nodes) == 1
+    assert contract_TestA.nodes[0].name == "test_foo"
+    assert contract_TestA.nodes[0].address == "0xdc24e7f1"
+
+    # contract C {
+    #     function foo() public pure returns (uint256) { ... }
+    # }
+    contract_C = mapper.get_by_name("C")
+    assert contract_C is not None
+    assert len(contract_C.nodes) == 1
+    assert contract_C.nodes[0].name == "foo"
+    assert contract_C.nodes[0].address == "0xc2985578"
