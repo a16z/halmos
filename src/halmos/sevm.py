b29b76676bdbc5b0c4019778ff4d561909940d1e
karmacoma <karma@coma.lol>
2024-08-16 16:19:11 -0700

    avoid str(cond) pattern

--- src/halmos/sevm.py
+++ src/halmos/sevm.py
@@ -541,7 +541,7 @@ class Path:
             [
                 f"- {cond}\n"
                 for cond in self.conditions
-                if self.conditions[cond] and str(cond) != "True"
+                if self.conditions[cond] and not is_true(cond)
             ]
         )
 
