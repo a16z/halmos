780b089f630141c5d85ca06d17c817f8cb008c2a
Daejun Park <daejunpark@gmail.com>
2024-10-07 12:30:43 -0700

    test

--- tests/regression/test/SymbolicCall.t.sol
+++ tests/regression/test/SymbolicCall.t.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Test.sol";
 
-import {IERC721TokenReceiver} from "forge-std/interfaces/IERC721.sol";
-
 contract C {
     function foo() public pure returns (bool) {
         return true;
