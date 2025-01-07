6fe84ee2148f31a4aa67d5f75cdc09e3447ca289
Daejun Park <daejunpark@gmail.com>
2025-01-07 12:15:45 -0800

    fix: not use context manager within generator (#434)

--- src/halmos/sevm.py
+++ src/halmos/sevm.py
@@ -15,6 +15,7 @@ from typing import (
     TypeVar,
 )
 
+import rich
 import xxhash
 from eth_hash.auto import keccak
 from rich.status import Status
@@ -1708,16 +1709,23 @@ class SEVM:
     storage_model: type[SomeStorage]
     logs: HalmosLogs
     steps: Steps
+    status: Status
 
     def __init__(self, options: HalmosConfig, fun_info: FunctionInfo) -> None:
         self.options = options
         self.fun_info = fun_info
         self.logs = HalmosLogs()
         self.steps: Steps = {}
+        self.status: Status = Status("")
 
         # init storage model
         is_generic = self.options.storage_layout == "generic"
         self.storage_model = GenericStorage if is_generic else SolidityStorage
+
+    def status_start(self) -> None:
+        # clear any remaining live display before starting a new instance
+        rich.get_console().clear_live()
+        self.status.start()
 
     def div_xy_y(self, w1: Word, w2: Word) -> Word:
         # return the number of bits required to represent the given value. default = 256
@@ -2666,10 +2674,6 @@ class SEVM:
         return ZeroExt(248, gen_nested_ite(0))
 
     def run(self, ex0: Exec) -> Iterator[Exec]:
-        with Status("") as status:
-            yield from self._run(ex0, status)
-
-    def _run(self, ex0: Exec, status: Status) -> Iterator[Exec]:
         step_id: int = 0
         stack: Worklist = Worklist()
         stack.push(ex0, 0)
@@ -2700,7 +2704,7 @@ class SEVM:
                     # hh:mm:ss
                     elapsed_fmt = timedelta(seconds=int(elapsed))
 
-                    status.update(
+                    self.status.update(
                         f"[{elapsed_fmt}] {speed:.0f} ops/s"
                         f" | completed paths: {stack.completed_paths}"
                         f" | outstanding paths: {len(stack)}"
