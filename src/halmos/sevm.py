3c248d571507ec8bb9f40d5bfd61feb22e38bcd7
karmacoma <karma@coma.lol>
2025-02-24 15:56:41 -0800

    delay jumpi_id call (533083 ops/s)

--- src/halmos/sevm.py
+++ src/halmos/sevm.py
@@ -2568,8 +2568,6 @@ class SEVM:
         stack: list[tuple[Exec, int]],
         step_id: int,
     ) -> None:
-        jid = ex.jumpi_id()
-
         target: int = ex.int_of(ex.st.pop(), "symbolic JUMPI target")
         cond = Bool(ex.st.pop())
 
@@ -2594,6 +2592,7 @@ class SEVM:
         follow_true = False
         follow_false = False
 
+        jid = ex.jumpi_id()
         visited = ex.jumpis.get(jid, {True: 0, False: 0})
 
         if potential_true and potential_false:
