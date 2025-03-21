83b54a6840f9218c555f011f8085ea0251169e96
karmacoma <karma@coma.lol>
2025-03-20 21:37:43 -0700

    feat: add support for ERC-1167 proxies (#483)

--- src/halmos/mapper.py
+++ src/halmos/mapper.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass, field
 from typing import Any, ForwardRef, Optional
 
 from .exceptions import HalmosException
-from .logs import warn
 
 SELECTOR_FIELDS = {
     "VariableDeclaration": "functionSelector",
@@ -169,7 +168,7 @@ class BuildOut(metaclass=SingletonMeta):
                 code_data = (hexcode, placeholders, contract_name, filename)
                 self._build_out_map_code[size].append(code_data)
 
-    def get_by_code(self, bytecode: ForwardRef("ByteVec")) -> tuple:
+    def get_by_code(self, bytecode: ForwardRef("ByteVec")) -> tuple[str, str]:
         """
         Return the contract name and file name of the given deployed bytecode.
         """
@@ -198,7 +197,6 @@ class BuildOut(metaclass=SingletonMeta):
             if eq_except_placeholders(hexcode, placeholders):
                 return (contract_name, filename)
 
-        warn(f"unknown deployed bytecode: {bytecode}")
         return (None, None)
 
     def get_by_name(self, contract_name: str, filename: str = None) -> dict:
