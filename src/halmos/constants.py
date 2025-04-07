80e70d12714baad15d9ebd6e0ed5002b3c32648e
karmacoma <karma@coma.lol>
2025-04-07 16:54:17 -0700

    deal with large vm.deal amounts
    fixes #338

--- src/halmos/constants.py
+++ src/halmos/constants.py
@@ -4,3 +4,7 @@ VERBOSITY_TRACE_PATHS = 4
 VERBOSITY_TRACE_CONSTRUCTOR = 5
 
 MAX_MEMORY_SIZE = 2**20
+
+# to be compatible with startHoax(address),
+# see https://github.com/a16z/halmos/issues/338
+MAX_ETH = 1 << 128
--- tests/regression/test/Deal.t.sol
+++ tests/regression/test/Deal.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "forge-std/Test.sol";
 
 contract DealTest is Test {
+    uint256 constant MAX_ETH = 1 << 128;
     C c;
 
     function check_deal_1(address payable receiver, uint amount) public {
@@ -24,6 +25,29 @@ contract DealTest is Test {
 
         assertGe(address(c).balance, 3 ether); // it is possible to send ether to c before it is created
         assert(address(this).balance == 0 ether);
+    }
+
+    // finds an empty counterexample along with a warning about the large balance
+    function check_deal_over_max_eth_concrete() public {
+        uint256 too_much = MAX_ETH + 1 ether;
+        vm.deal(address(this), too_much);
+        assertNotEq(address(this).balance, too_much);
+    }
+
+    function check_deal_over_max_eth_mixed(address addr) public {
+        uint256 too_much = MAX_ETH + 1 ether;
+        vm.deal(address(this), too_much);
+
+        // cex won't be found because of the constraint on large balances < MAX_ETH
+        assertLe(addr.balance, MAX_ETH);
+    }
+
+    function check_deal_under_max_eth_mixed(address addr) public {
+        uint256 large_but_ok = MAX_ETH - 1 ether;
+        vm.deal(address(this), large_but_ok);
+
+        // cex can be found
+        assertLe(addr.balance, MAX_ETH / 2);
     }
 }
 
