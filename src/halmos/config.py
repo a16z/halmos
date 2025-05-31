dd616bd340372f78411742d6e3caf062b3a4432b
karmacoma <karma@coma.lol>
2025-05-30 17:32:08 -0700

    Update src/halmos/config.py
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- src/halmos/config.py
+++ src/halmos/config.py
@@ -476,7 +476,7 @@ class Config:
     ### Solver options
 
     solver: str = arg(
-        help="specify the SMT solver to use (e.g., 'yices'). If not specified, defaults to 'z3'. If --solver-command is used, this is ignored.",
+        help="specify the SMT solver to use. If not specified, defaults to 'yices'. If --solver-command is used, this is ignored.",
         global_default="yices",
         # skip metavar, otherwise argparse does not show the choices
         # metavar="SOLVER_NAME",
