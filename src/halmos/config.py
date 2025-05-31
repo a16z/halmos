527bc2ad2b37bf772dd420a1fa6ba6e2a4934aae
karmacoma <karma@coma.lol>
2025-05-30 17:23:34 -0700

    switch default solver to yices

--- src/halmos/config.py
+++ src/halmos/config.py
@@ -477,7 +477,7 @@ class Config:
 
     solver: str = arg(
         help="specify the SMT solver to use (e.g., 'yices'). If not specified, defaults to 'z3'. If --solver-command is used, this is ignored.",
-        global_default="z3",
+        global_default="yices",
         # skip metavar, otherwise argparse does not show the choices
         # metavar="SOLVER_NAME",
         choices=list(SOLVERS.keys()),
